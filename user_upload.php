7f3aaa07cd842ac9e049d7d12e1746952010c718
Josh Zhu <zhjjosh@hotmail.com>
2019-02-10 20:28:03 +1100

    finalise the user_upload.php file

--- user_upload.php
+++ user_upload.php
@@ -5,10 +5,23 @@ require_once "src/ScriptHandler.php";
 $scriptHandler = new ScriptHandler($argv);
 $argList = $scriptHandler->getArgList();
 
-
-$scriptHandler->testConnection();
-if ($argList['create_table']){
-	$scriptHandler->createTables();
+if ($argList['help']){
+	echo $scriptHandler->getHelp();
+}else{
+	$scriptHandler->testConnection();
+	if ($argList['create_table']){
+		$scriptHandler->createTables();
+	}else{
+		$fileArray = $scriptHandler->readCsvFile($argList['filename']);
+		if (empty($fileArray)){
+			echo "No record is inserted. \n";
+		}else{
+			if (!$argList['dry_run']){
+				$scriptHandler->insertIntoDB($fileArray);
+				echo sizeof($fileArray)." records are inserted. \n";
+			}
+		}
+	}
 }
 
 
